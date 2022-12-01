9b39e89604de686617651044eb22dd91e5585a6c
sandeepPangare <pangaresandeep96@gmail.com>
2022-12-01 22:22:40 +0530

    Added:User Registration System Problem

---
+++ src/main/java/com/bridgelabz/UserRegistrationMain.java
@@ -0,0 +1,21 @@
+package com.bridgelabz;
+
+import java.util.Scanner;
+
+public class UserRegistrationMain {
+
+    public static void welcome(){
+        System.out.println("Welcome to User Registration System Problem");
+    }
+    public static void main(String[] args) {
+        welcome();
+        UserRegistration user = new UserRegistration();
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter 1 : to validate First Name ");
+        switch (scanner.nextInt()){
+            case 1:
+                UserRegistration.validFirstName();
+                break;
+        }
+    }
+}
